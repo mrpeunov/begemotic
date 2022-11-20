84e31644a59f1af2c852125fa30dd0f0a5f7b06c
Vitaly <mr.peunov@gmail.com>
2022-11-20 18:06:21 +0300

    Добавил расчёт h3 для точки

--- src/app/services/indexer/point.py
+++ src/app/services/indexer/point.py
@@ -1,9 +1,18 @@
+import h3
+
 from .. import entries
 from .base import BaseIndexer
+from loguru import logger
+from src.core import config
 
 
 class PointIndexer(BaseIndexer):
     def index(self, geopos: entries.PointGeopos) -> entries.H3Indexes:
-        return [
-            entries.H3Index('822d57fffffffff')
-        ]
+        main_index = h3.geo_to_h3(
+            lat=geopos.geometry.coordinates[0],
+            lng=geopos.geometry.coordinates[1],
+            resolution=config.RESOLUTION
+        )
+        indexes = h3.k_ring(h=main_index, k=geopos.r)
+        logger.info(f"Geopos: {geopos}; H3: {indexes}")
+        return [entries.H3Index(index) for index in indexes]
--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+import sys
+
 from fastapi import FastAPI
 
 from src.api.handlers import router
+from loguru import logger
+
 
 app = FastAPI()
 app.include_router(router)
+logger.add(sys.stdout, format="{time} {level} {message}", filter="my_module", level="INFO")
